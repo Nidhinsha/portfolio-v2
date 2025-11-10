3c11bf5c167363141744e513a70bc04957d7253c
Nidhinsha <nidhinsha64@gmail.com>
2025-11-10 20:46:00 +0530

    fix: removed basename of root

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: `/`,
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
       host: false, // Allow access from network devices
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
-      },
     },
   };
 });
